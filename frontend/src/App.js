f876233ed015f0abe8c842c124bfea0ffa0842a6
Tanuj Patra <tanujpatra228@gmail.com>
2022-09-04 21:56:13 +0530

    Ep #2 - Folder structure & New Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Hello />
         </p>
         <a
           className="App-link"
---
+++ frontend/src/components/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hello = () => {
+    return <h1>Hello Tanuj</h1>
+}
+
+export default Hello;
