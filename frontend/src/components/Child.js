dad77d2970ec068dce4129459924206149a69f66
Tanuj Patra <tanujpatra228@gmail.com>
2022-10-02 19:18:10 +0530

    Ep #5 - useEffect Hook

--- frontend/src/components/Child.js
+++ frontend/src/components/Child.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Child = () => {
+    useEffect(()=>{
+        console.log('Child');
+    }, []);
     return <h1>Hi from child</h1>
 }
 export default Child;
--- frontend/src/components/Hello.js
+++ frontend/src/components/Hello.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './css/style.module.css';
 import Child from './Child';
 
 const Hello = (props) => {
     const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
 
-    console.log('fname', fname);
+    useEffect(()=>{
+        console.log('Hello');
+    }, [fname]);
+    
+    useEffect(()=>{
+        console.log('Hello 2');
+    });
+
     return(
         <>
             <h1 className={style.h1}>Hello {props.name}</h1>
             <Child />
             <p className={style.p}>{fname}</p>
             <input type="text" onChange={(e) => setFname(e.target.value)} value={fname} />
+            <input type="text" onChange={(e) => setLname(e.target.value)} value={lname} />
         </>
     );
 }
