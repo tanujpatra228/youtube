80014b91dca09817a9e678fbea16dc851568dc5e
Tanuj Patra <tanujpatra228@gmail.com>
2023-03-29 21:29:02 +0530

    Ep #14 - DEPLOY WordPress and React JS on SHARED HOSTING

--- frontend/src/components/common/Navbar/index.js
+++ frontend/src/components/common/Navbar/index.js
@@ -5,6 +5,12 @@ const Navbar = () => {
   const auth = localStorage.getItem('user');
   return (
     <div className='p-5 '>
+      <div className='max-w-5xl mx-auto flex justify-between items-center py-2'>
+
+        <div>
+          <Link to='/' className='text-2xl font-semibold'>IWS</Link>
+        </div>
+
         <ul className='flex gap-5 justify-end'>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/posts'>Posts</Link></li>
@@ -17,6 +23,8 @@ const Navbar = () => {
               </>
             )}
         </ul>
+        
+      </div>
     </div>
   )
 }
--- frontend/src/components/pages/Home/index.js
+++ frontend/src/components/pages/Home/index.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <h1 className='text-2xl font-bold'>Home</h1>
+    <>
+      <section className="flex flex-col justify-center items-center bg-gray-100 h-screen">
+        <div className="max-w-4xl mx-auto text-center">
+          <h1 className="text-4xl font-bold mb-4">Welcome to My Blog</h1>
+          <p className="text-lg text-gray-600 mb-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link
+            to="/posts"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full transition-colors duration-300 ease-in-out"
+          >
+            Visit the Blog
+          </Link>
+        </div>
+      </section>
+    </>
   )
 }
 
