8477d8f9add5782bddf9bfe4b77d8cf66f33b43b
Tanuj Patra <tanujpatra228@gmail.com>
2023-05-08 23:18:24 +0530

    Ep #18 - Create PAGINATION in React JS

--- frontend/src/components/pages/Posts/index.js
+++ frontend/src/components/pages/Posts/index.js
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const perPage = 9;
     useEffect(()=>{
-        let url = `${process.env.REACT_APP_API_ROOT}/posts`;
+        let url = `${process.env.REACT_APP_API_ROOT}/posts?per_page=${perPage}&page=${currentPage}`;
         axios.get(url).then((res)=>{
-            setPosts(res.data);
+            const { data, headers } = res;
+            setTotalPages(Number(headers['x-wp-totalpages']));
+            setPosts(data);
         });
-    }, []);
+    }, [currentPage]);
     console.log('posts', posts);
 
     return(
@@ -30,6 +35,25 @@ const Posts = () => {
                 )
             }) : 'Loading...'
         }
+        </div>
+        
+        {/* Pagination */}
+        <div className='w-3/12 py-10 m-auto flex justify-between items-center align-middle flex-wrap gap-10'>
+            <button className='btn-primary p-2 bg-blue-500 text-white text-lg rounded-lg hover:shadow-lg disabled:opacity-50'
+                disabled={currentPage === 1}
+                onClick={() => setCurrentPage(currentPage-1)}
+            >
+                Previous
+            </button>
+
+            <span className='text-lg'>{currentPage} of {totalPages}</span>
+            
+            <button className='btn-primary p-2 bg-blue-500 text-white text-lg rounded-lg hover:shadow-lg disabled:opacity-50'
+                disabled={currentPage === totalPages} 
+                onClick={() => setCurrentPage(currentPage+1)}
+            >
+                Next
+            </button>
         </div>
         </>
     )
