505e504568be968c32fa2a02431f65eb82fe570f
Tanuj Patra <tanujpatra228@gmail.com>
2023-05-29 21:48:38 +0530

    Ep #20 - Update Tailwing CSS: line-clamp-[n]

--- frontend/src/components/common/Card/index.js
+++ frontend/src/components/common/Card/index.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Card = ({post}) => {
   return (
     <>
-      <div key={post.id} className='card bg-white p-3 w-full shadow-lg rounded-lg flex gap-5 flex-col transition duration-300 ease-in-out hover:shadow-2xl'>
-        <Link to={`/posts/${post.id}`}>
+      <div key={post.id} className='card bg-white p-3 w-full shadow-lg rounded-lg transition duration-300 ease-in-out hover:shadow-2xl'>
+        <Link to={`/posts/${post.id}`} className="space-y-5">
           <img src={post.featured_src ? post.featured_src : 'https://via.placeholder.com/500'} alt={post.title.rendered} className="w-full h-64 object-cover" />
-          <h2 className='text-lg font-bold'>{post.title.rendered}</h2>
-          <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
+          <h2 className='text-lg font-bold line-clamp-1'>{post.title.rendered}</h2>
+          <p className='line-clamp-3' dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
         </Link>
       </div>
     </>
